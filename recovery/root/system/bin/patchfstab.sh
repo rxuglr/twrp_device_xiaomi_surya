6481bc79c2bb5134605c34f2c8b9ca82d459adf9
rxuglr <69907487+rxuglr@users.noreply.github.com>
2022-12-04 20:35:09 +0300

    Update universal decryption script

--- recovery/root/system/bin/patchfstab.sh
+++ recovery/root/system/bin/patchfstab.sh
@@ -1,11 +1,21 @@
 #!/system/bin/sh
+#
 # This script is needed to support wrappedkey and fbe v2
+# Version: 1.1
+# Date : 2022/12/03
 # Author : brigudav
+# strings fixed by rxuglr
 #
 
-n=1
+reclog=/tmp/recovery.log
+tmpv=/tmp/vendor_fstab
+fstabd=$tmpv/etc/fstab.default
+fstabq=$tmpv/etc/fstab.qcom
+fstabr=/system/etc/recovery.fstab
 
-while [ $n -le 2 ]; do
+send_log() {
+	echo "I:$1" >> $reclog
+}
 
 finish() {
 	sleep 1
@@ -14,39 +24,32 @@ finish() {
 	exit 1
 }
 
-logf=/tmp/recovery.log
-tmpv=/tmp/vendor_fstab
-mkdir -p $tmpv
-fstabd=$tmpv/etc/fstab.default
-fstabq=$tmpv/etc/fstab.qcom
-fstabr=/system/etc/recovery.fstab
-mount -o ro -t auto /dev/block/mapper/vendor $tmpv || true
-if [ -f $fstabq ]; then
-	fbev2=$(grep "/userdata" ${fstabq} | grep ":v2")
-	echo "I:Find $fstabq" >> $logf
-	if [ -z "$fbev2" ]; then
-		echo "I:This ROM not support FBE v2. Changing flags in fstab" >> $logf
-		cp $fstabr.bak $fstabr
-		wrapv=$(grep "/userdata" ${fstabq} | grep "wrappedkey")
-		if [ -n "$wrapv" ]; then
-			echo "I:And enabled supports wrappedkey..." >> $logf
-			sed -i -e "s/formattable/formattable,wrappedkey/g" $fstabr
+patch_fstab() {
+	mkdir -p $tmpv
+	mount -o ro -t auto /dev/block/mapper/vendor $tmpv || true
+
+	for i in $fstabq $fstabd; do
+		if [ -f $i ]; then
+		send_log "Find $i"
+			if [[ ! $(grep "/userdata" $i | grep ":v2") = "" ]]; then
+				send_log "FBE v2 support found..."
+				finish
+			else
+				send_log "Enabling FBE v1 support..."
+				cp $fstabr.bak $fstabr
+				if [[ ! $(grep "/userdata" $i | grep wrappedkey) = "" ]]; then
+					send_log "...and enabling wrappedkey."
+					sed -i -e "s/formattable/formattable,wrappedkey/g" $fstabr
+				fi
+				finish
+			fi
 		fi
-		finish
-	else
-		echo "I:This ROM with FBE v2. Continuing with the default fstab." >> $logf
-		finish
-	fi
-elif [ -f $fstabd ]; then
-	cp $fstabr.bak $fstabr
-	wrap=$(grep "/userdata" ${fstabd} | grep "wrappedkey")
-	echo "I:Find $fstabd" >> $logf
-	if [ -n "$wrap" ]; then
-		echo "I:This ROM supports wrappedkey. Changing flags in fstab" >> $logf
-		sed -i -e "s/formattable/formattable,wrappedkey/g" $fstabr
-	fi
-	finish
-fi
+	done
+}
+
+n=1
+while [ $n -le 2 ]; do
+	patch_fstab
 sleep 1
 done
 
